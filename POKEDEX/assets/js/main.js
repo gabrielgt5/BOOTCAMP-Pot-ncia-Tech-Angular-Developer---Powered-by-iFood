37ff4a17e823154ee871ecc9dfa57425021ec581
Gabriel Lucas <69332675+gabrielgt5@users.noreply.github.com>
2023-05-06 10:44:29 -0400

    new commit

---
+++ POKEDEX/assets/js/main.js
@@ -0,0 +1,31 @@
+
+
+//Renderizando o HTML lista de Pokemons
+function convertPokemonToHtml(pokemon) {
+    return`
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonOl = document.getElementById('pokemonList');
+
+
+
+    poke.then((pokemonList) => {
+        for(i=0; i <pokemonList.lenght; i++){
+            const pokemon = pokemonList[i];
+            pokemonOl.innerHTML += convertPokemonToHtml(pokemon);
+            
+        }
+    })
+    .catch((error) => console.error(error))
